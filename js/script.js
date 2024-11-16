92326cd29dace1f7ae050b8b06fa3bb2011176ef
amanda-bann <amanda_klap@protonmail.com>
2024-11-15 23:26:32 -0700

    Create an event that will display the vehicles that need repairing when a button is clicked

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const button = document.querySelector("button");
+const repairList = document.querySelector(".need-repair");
+
 const createVehicle = function (type, numWheels, color) {
   const vehicle = {};
   vehicle.type = type;
@@ -18,3 +21,21 @@ for (let vehicle of myVehicles) {
     console.log(key, vehicle[key]);
   }
 }
+
+bike.needsRepair = true;
+skateboard.needsRepair = true;
+
+//clear list
+button.addEventListener("click", function () {
+  repairList.innerHTML = ""; 
+
+//get list of repairs
+  const vehicleRepairList = myVehicles.filter(function(vehicle) {
+    return vehicle.needsRepair === true;
+});
+  for (let vehicle of vehicleRepairList) {
+    let li = document.createElement("li");
+    li.innerHTML = `My <span>${vehicle.type}</span> needs some 💛`;
+    repairList.append(li);
+  }
+});
